e34557ab3e4fb38d2e454359078b4170f86a2658
HoangDuy1999 <tranhoangduy.911@gmail.com>
2024-07-11 16:20:53 +0700

    feat: initial project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Counter } from './Counter'
+
+export const App = () => {
+  return (
+    <>
+      <h1>React </h1>
+      <Counter />
+    </>
+  )
+}
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack')
+const path = require('path')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    hot: true,
+    open: true,
+    contentBase: path.resolve(__dirname, '..', './build'),
+  },
+  plugins: [
+    new ReactRefreshWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('Vishwas'),
+    }),
+  ],
+}
